9508cc73413197f43cda65d5e67952c155af7109
Boscoliu1994 <yal144@ucsd.edu>
2025-05-24 14:58:58 -0700

    Completed Lab 8 - fetch, service workers, and PWA setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Lab8-Starter
+# Lab 8 - Network & ServiceWorkers
+
+## Team Members
+- Bosco Liu
+
+## Deployed GitHub Pages Link
+https://your-username.github.io/Lab8_Starter/
+
+## How are graceful degradation and service workers related?
+
+Graceful degradation means building apps that continue to function even when some features aren't supported. Service workers support graceful degradation by allowing apps to function offline or in poor network conditions by caching key resources, ensuring core functionality remains accessible even without internet.
+
+## PWA
+![PWA Screenshot](pwa.png)
--- sw.js
+++ sw.js
@@ -9,7 +9,30 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/scripts/main.js',
+        '/assets/styles/main.css',
+        '/assets/scripts/RecipeCard.js',
+        '/assets/images/icons/0-star.svg',
+        '/assets/images/icons/1-star.svg',
+        '/assets/images/icons/2-star.svg',
+        '/assets/images/icons/3-star.svg',
+        '/assets/images/icons/4-star.svg',
+        '/assets/images/icons/5-star.svg',
+        '/assets/images/icons/arrow-down.png',
+        '/assets/images/icons/icon-192x192.png',
+        '/assets/images/icons/icon-256x256.png',
+        '/assets/images/icons/icon-384x384.png',
+        '/assets/images/icons/icon-512x512.png',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+      ]);
     })
   );
 });
@@ -34,7 +57,24 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      // B8. TODO - If the request is in the cache, return with the cached version.
+      //            Otherwise fetch the resource, add it to the cache, and return
+      //            network response.
+      return cache.match(event.request).then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        }).catch(() => {
+          return new Response('offline and this rescource is not cached.', {
+            headers: { 'Content-Type': 'text/plain' }
+          });
+        });
+      });
+    })
+  );
 });
